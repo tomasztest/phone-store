17dbee4719647b3920b4f28fef137890dd4cda6a
tomasztest <tomaszkotzur@gmail.com>
2019-10-04 06:57:39 +0200

    first commit

---
+++ src/data.js
@@ -0,0 +1,36 @@
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Google Pixel - Black",
+    img: "img/pixel.jpeg",
+    price: 10,
+    company: "GOOGLE",
+    info: "Lorem ipsum",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Nokia",
+    img: "img/nokia.jpg",
+    price: 34,
+    company: "Nokia",
+    info: "Lorem ipsum",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Google Pixel - Black",
+  img: "img/pixel.jpeg",
+  price: 10,
+  company: "google",
+  info: "Lorem ipsum google 2",
+  inCart: false,
+  count: 0,
+  total: 0
+};
